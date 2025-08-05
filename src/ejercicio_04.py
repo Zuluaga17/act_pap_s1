3999244d5fbf111f991ed2aa75101de1b0183696
Zuluaga17 <zuluaga110922@gmail.com>
2025-08-05 16:29:23 -0500

    actividad 4

---
+++ src/ejercicio_04.py
@@ -0,0 +1,6 @@
+total = 0
+num = int(input("Ingrese un número (0 para terminar): "))
+while num != 0:
+    total += num
+    num = int(input("Ingrese un número (0 para terminar): "))
+print("Suma total:", total)
